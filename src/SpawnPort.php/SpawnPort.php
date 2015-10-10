7366f1e75edb22b74a5e64f1ededa612821d3ee4
Ronald Schmidt <ronald.schmidt@zu-web.de>
2015-10-10 12:36:51 +0200

    add spawn command

--- src/SpawnPort.php/SpawnPort.php
+++ src/SpawnPort.php/SpawnPort.php
@@ -5,11 +5,11 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 
 class SpawnPort extends PluginBase{
 
     public function onEnable(){
-        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
     }
 
     public function onDisable(){
@@ -18,9 +18,13 @@ class SpawnPort extends PluginBase{
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
         if(strtolower($command->getName()) === "spawnport"){
             if(!($sender instanceof Player)){
-                $sender->sendMessage("Please run this command in game");
+                $sender->sendMessage(TextFormat::AQUA."Please run this command in game");
                 return true;
             }
+
+            $worldspawn = $sender->getLevel()->getSpawnLocation();
+            $sender->teleport($worldspawn);
+            $sender->sendMessage(TextFormat::AQUA."Teleported to WorldSpawn");
         }
         return true;
     }
